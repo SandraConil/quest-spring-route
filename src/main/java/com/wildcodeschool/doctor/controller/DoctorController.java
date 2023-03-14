a7c7335aef5bac7e8885b04c31408c17ec8793c8
SandraConil <[sandra.conil@sncf.fr](mailto:sandra.conil@sncf.fr)>
2023-03-14 11:48:14 +0100

    final

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,38 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
-
-    @GetMapping("/doctor/")
+    /*@GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
+    public String doctor(@PathVariable int number) {
 
-        return null;
+        return "doctor" + number;
+    }*/
+
+    /*@GetMapping("/doctor/13")
+    @ResponseBody
+    public Doctor doctor13() {
+        return new Doctor(13,"Jodie Whittaker");
+    }*/
+
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    Doctor getDoctor(@PathVariable int number){
+        if (number >0 && number < 13){
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "ERROR 303");
+        }else if (number == 13) {
+            return new Doctor(13,"Jodie Whittaker");
+        }
+        else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ERROR 404, Impossible de récuperer l'incarnation");
+        }
     }
 }
